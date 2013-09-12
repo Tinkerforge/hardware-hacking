cf9ba9fae8186c238ab7422c24ce89fe40d72d0f
Matthias Bolte <matthias@tinkerforge.com>
2013-09-12 17:45:42 +0200

    ios: Add basic Garage Control app

---
+++ garage_control_smart_phone/ios/GarageControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GarageControl
+//
+//  Created by Olaf Lüke on 12.09.13.
+//  Copyright (c) 2013 Olaf Lüke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
