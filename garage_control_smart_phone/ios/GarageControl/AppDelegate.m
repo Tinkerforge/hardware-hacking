477b994e3a031c335a1d159eed8c511840ebe320
Matthias Bolte <matthias@tinkerforge.com>
2013-10-18 17:52:22 +0200

    ios: Add program logic to Garage Control app

--- garage_control_smart_phone/ios/GarageControl/AppDelegate.m
+++ garage_control_smart_phone/ios/GarageControl/AppDelegate.m
@@ -19,6 +19,7 @@
     self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+
     return YES;
 }
 
@@ -32,6 +33,7 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+   [self.viewController saveState];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -42,6 +44,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   [self.viewController restoreState];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
--- garage_control_smart_phone/ios/GarageControl/ViewController.h
+++ garage_control_smart_phone/ios/GarageControl/ViewController.h
@@ -7,7 +7,35 @@
 //
 
 #import <UIKit/UIKit.h>
+#include "ip_connection.h"
+#include "bricklet_industrial_quad_relay.h"
 
 @interface ViewController : UIViewController
+{
+    IBOutlet UITextField *hostTextField;
+    IBOutlet UITextField *portTextField;
+    IBOutlet UITextField *uidTextField;
+    IBOutlet UIButton *connectButton;
+    IBOutlet UIButton *triggerButton;
+    IBOutlet UIActivityIndicatorView *indicator;
+
+    dispatch_queue_t queue;
+    IPConnection ipcon;
+    IndustrialQuadRelay relay;
+    BOOL connected;
+}
+
+@property (nonatomic, retain) UITextField *hostTextField;
+@property (nonatomic, retain) UITextField *portTextField;
+@property (nonatomic, retain) UITextField *uidTextField;
+@property (nonatomic, retain) UIButton *connectButton;
+@property (nonatomic, retain) UIButton *triggerButton;
+@property (nonatomic, retain) UIActivityIndicatorView *indicator;
+
+- (IBAction)connectPressed:(id)sender;
+- (IBAction)triggerPressed:(id)sender;
+
+- (void)saveState;
+- (void)restoreState;
 
 @end
