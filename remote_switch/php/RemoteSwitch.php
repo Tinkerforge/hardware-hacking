73ee1aa701485c029703bb9eb1d2c2479229ecfd
Matthias Bolte <matthias@tinkerforge.com>
2014-01-15 11:50:09 +0100

    php: Fix example

--- remote_switch/php/RemoteSwitch.php
+++ remote_switch/php/RemoteSwitch.php
@@ -9,10 +9,10 @@ use Tinkerforge\BrickletIndustrialQuadRelay;
 const HOST = 'localhost';
 const PORT = 4223;
 const UID = 'ctG'; // Change to your UID
-const VALUE_A_ON  = (1 << 0) | (1 << 2); // Pin 0 and 2 high
-const VALUE_A_OFF = (1 << 0) | (1 << 3); // Pin 0 and 3 high
-const VALUE_B_ON  = (1 << 1) | (1 << 2); // Pin 1 and 2 high
-const VALUE_B_OFF = (1 << 1) | (1 << 3); // Pin 1 and 3 high
+$VALUE_A_ON  = (1 << 0) | (1 << 2); // Pin 0 and 2 high
+$VALUE_A_OFF = (1 << 0) | (1 << 3); // Pin 0 and 3 high
+$VALUE_B_ON  = (1 << 1) | (1 << 2); // Pin 1 and 2 high
+$VALUE_B_OFF = (1 << 1) | (1 << 3); // Pin 1 and 3 high
 
 $ipcon = new IPConnection(); // Create IP connection
 $iqr = new BrickletIndustrialQuadRelay(UID, $ipcon); // Create device object
@@ -20,7 +20,7 @@ $iqr = new BrickletIndustrialQuadRelay(UID, $ipcon); // Create device object
 $ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
-$iqr->setMonoflop(VALUE_A_ON, 15, 1500);
+$iqr->setMonoflop($VALUE_A_ON, 15, 1500);
 
 $ipcon->disconnect();
 
