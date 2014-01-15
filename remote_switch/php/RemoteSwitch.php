21d39c45502b9ad6cb8486bd9b86093e7217bf3a
Matthias Bolte <matthias@tinkerforge.com>
2014-01-15 11:39:33 +0100

    php: Use const for constant values

--- remote_switch/php/RemoteSwitch.php
+++ remote_switch/php/RemoteSwitch.php
@@ -6,21 +6,21 @@ require_once('Tinkerforge/BrickletIndustrialQuadRelay.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickletIndustrialQuadRelay;
 
-$host = 'localhost';
-$port = 4223;
-$uid = 'ctG'; // Change to your UID
-$VALUE_A_ON  = (1 << 0) | (1 << 2); // Pin 0 and 2 high
-$VALUE_A_OFF = (1 << 0) | (1 << 3); // Pin 0 and 3 high
-$VALUE_B_ON  = (1 << 1) | (1 << 2); // Pin 1 and 2 high
-$VALUE_B_OFF = (1 << 1) | (1 << 3); // Pin 1 and 3 high
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = 'ctG'; // Change to your UID
+const VALUE_A_ON  = (1 << 0) | (1 << 2); // Pin 0 and 2 high
+const VALUE_A_OFF = (1 << 0) | (1 << 3); // Pin 0 and 3 high
+const VALUE_B_ON  = (1 << 1) | (1 << 2); // Pin 1 and 2 high
+const VALUE_B_OFF = (1 << 1) | (1 << 3); // Pin 1 and 3 high
 
 $ipcon = new IPConnection(); // Create IP connection
-$iqr = new BrickletIndustrialQuadRelay($uid, $ipcon); // Create device object
+$iqr = new BrickletIndustrialQuadRelay(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
-$iqr->setMonoflop($VALUE_A_ON, 15, 1500);
+$iqr->setMonoflop(VALUE_A_ON, 15, 1500);
 
 $ipcon->disconnect();
 
