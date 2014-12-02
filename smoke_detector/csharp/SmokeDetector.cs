492b39f67b779e5f5de1a69a467dc0f7739def03
Olaf Lüke <olaf@tinkerforge.com>
2014-12-02 17:30:18 +0100

    Use ReadLine instead of ReadKey for C# example

--- smoke_detector/csharp/SmokeDetector.cs
+++ smoke_detector/csharp/SmokeDetector.cs
@@ -97,8 +97,8 @@ class SmokeDetector
 			}
 		}
 
-		System.Console.WriteLine("Press key to exit");
-		System.Console.ReadKey();
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
 		ipcon.Disconnect();
 	}
 }
